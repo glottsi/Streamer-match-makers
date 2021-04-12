e1f51b73057a6cc52941cf85ae28bf7b729cf0f8
glottsi <glottsifromtwitch@gmail.com>
2021-04-12 11:39:23 -0400

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 // routes
-app.use("/about", aboutRouter);
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
